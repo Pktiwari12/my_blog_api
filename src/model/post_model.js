9adb1d2ee067e6469a68386104b185e5958a39a1
Pktiwari12 <pradeepkumargarautha2005@gmail.com>
2025-01-27 22:44:26 +0530

    The basic schema of post is implemented

--- src/model/post_model.js
+++ src/model/post_model.js
@@ -2,8 +2,19 @@ import mongoose from "mongoose";
  const postSchema = mongoose.Schema({
     title: {
         type: String,
-        required: true
+        required: [true , "title is required"],
+        maxlength: 100
+    },
+    content:{
+        type: String,
+        required: true,
+
+    },
+    author:{
+        type: String,
+        required: true,
     }
+    
 })
 
-export default mongoose.model("post",postSchema);
+export default mongoose.model("user-posts",postSchema);
