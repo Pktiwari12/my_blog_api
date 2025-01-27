5feb7162f3f0f7d9f61680d884cf8c4cc72d4df1
Pktiwari12 <pradeepkumargarautha2005@gmail.com>
2025-01-27 23:51:31 +0530

    implimentation of update the task is implemented

--- src/controllers/post_controllers.js
+++ src/controllers/post_controllers.js
@@ -1,8 +1,9 @@
 import Post from "./../model/post_model.js";
 export const createPost = async (req ,res) =>{
     try{
-        const postData = new Post(req.body);
-        const savedPost =  postData.save();
+        const{title , content , author} = req.body;
+        const newPost = new Post({title ,content, author })
+        const savedPost = await  newPost.save();
         res.status(201).json({message: "post is saved in database"});
 
     }catch(error){
@@ -24,3 +25,23 @@ export const getAllPosts = async (req , res) =>{
         res.status(500).json({error: "Internel Server is occured for getting all posts"});
     }
 };
+
+export const updatePost = async (req , res) =>{
+    try{
+        const {title , content , author} = req.body;
+        const post_id = req.params.id;
+        const postAvailable = await Post.findOne({_id: post_id});
+        if(!postAvailable){
+            res.status(404).json({message : "The is no such post available."});
+        }
+        else{
+            postAvailable.title = title ? title : postAvailable.title;
+            postAvailable.content = content;
+            postAvailable.author = author;
+            const updatedPost = await postAvailable.save();
+            res.status(201).json({message: "the post is updated.", post: updatedPost});
+        }
+    }catch(error){
+        res.status(500).json({error: "Internel Error is occured."});
+    }
+}
--- src/routes/post_route.js
+++ src/routes/post_route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { createPost, getAllPosts } from "../controllers/post_controllers.js";
+import { createPost, getAllPosts,updatePost } from "../controllers/post_controllers.js";
 
 const route = express.Router();
 route.post("/create",createPost);
 route.get("/all-posts",getAllPosts);
+route.put("/update-post/:id",updatePost);
 export default route
