b1fb8d6447acbd0d6a7d60aba0e4e56e520ddf4f
Pktiwari12 <pradeepkumargarautha2005@gmail.com>
2025-01-27 19:45:39 +0530

    The db is connected and post is inserted through postman

--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
+import dotenv from "dotenv";
+// Loading the environment variable
+dotenv.config();
+console.log("Environment varibale is loaded.");
 import express from "express";
+import bodyParser from "body-parser";
+import  connectDB  from "./init/db.js";
+import route from "./routes/post_route.js"
+// try{
+//     await mongoose.connect(process.env.MONGODB_URL);
+//     console.log("Database is connected ");
+// }catch(error){
+//     console.log("database-connection is failed"+error.message);
+// }
+connectDB().then(()=>{
+    console.log("db is conneced.");
+})
 const app = express();
+app.use(bodyParser.json());
+app.use("/api/post",route);
 export default app;
+
+
--- src/server.js
+++ src/server.js
@@ -1,8 +1,4 @@
 import app from "./app.js";
-import dotenv from "dotenv";
-// Loading the environment variable
-dotenv.config();
-console.log("Environment varibale is loaded.");
 console.log(process.env.PORT);
 const port = process.env.PORT;
 app.listen(port , ()=>{
