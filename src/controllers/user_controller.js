6ac9a2b2c3ab30464a8a0fff198e9ece40d73e93
Pktiwari12 <pradeepkumargarautha2005@gmail.com>
2025-02-10 16:18:47 +0530

    only verification link sendig feature is added.

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -1,6 +1,7 @@
 import User from "./../model/user_model.js";
 import bcrypt from "bcrypt";
 import {validationResult} from "express-validator";
+import { sendEmail } from "../utils/mailer.js";
 // anonymous function to create user in database
 const register = async (req , res) =>{
     try{
@@ -29,10 +30,28 @@ const register = async (req , res) =>{
             password: password
         })
     }
+    // checking Whether user exist or not
+    const isEmailExist = await User.findOne({email: email});
+    if(isEmailExist){
+         return res.status(400).json({
+                success: false,
+                msg: "Email is already exist",
+        })
+    }
     const hashedPassword = await bcrypt.hash(password , 10);
     const file = req.files.profileImage[0];
     const newUser = new User({name , email , password: hashedPassword, profileImage: "./../public/image/"+file.filename}, role);
+    // after the sign up of user , the verifcation link is sended to his mail
+    try {
+        const mailMessage = '<p>Hi '+newUser.name +'please <a href = "http://localhost:3000/mail-verification/?id='+newUser._id+'">verify</a> your email </p>';
+        sendEmail(email, "Email Verification", mailMessage);
+    } catch (error) {
+        console.log(error);
+    }
+
     const savedUser = await newUser.save();
+    
+
     console.log(`The saved user is ${savedUser}`);  
     res.status(201).json({
         success: true,
---
+++ src/utils/mailer.js
@@ -0,0 +1,51 @@
+import dotenv from "dotenv";
+dotenv.config();
+console.log("Environment variaable is loaded in utils/mailer.js"+"Which is not good thing to import this file");
+import nodemailer from "nodemailer";
+// console.log(process.env.SMTP_MAIL);
+
+// First of all , transport is created.
+const transport = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    auth: {
+        user: process.env.SMTP_MAIL,
+        pass: process.env.SMTP_PASSWORD
+
+    }
+});
+
+const sendEmail = async (email , subject , content) =>{
+    try{
+        // defining object of mail option 
+        const mailOptions = {
+            from: process.env.SMTP_MAIL,
+            to: email,
+            subject: subject,
+            html: content
+        };
+        // then the passing the object of message into the transporter
+        transport.sendMail(mailOptions , (error , info) =>{
+            if(error){
+                console.log("error");
+                throw error;
+            }
+            else{
+                console.log("Message Sent",info.id);
+            }
+        })
+
+    }catch(error){
+        console.log(error);
+    }
+}
+
+export {sendEmail};
+
+
+
+// Note : - here we follow the two Step 
+// 1. creating the transport and strore it transport variable
+// 2. defining the anonymous function to send mail.
